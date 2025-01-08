0d2b9c8a0b2a81a9e6540a0440c04de38efc1316
Le-Duc-Nguyen-10 <nguyenldpd10357@gmail.com>
2025-01-08 10:36:08 +0700

    bai2 lab1

---
+++ soccer.js
@@ -0,0 +1,19 @@
+const players = [
+    { name: 'Messi', goals: 30 },
+    undefined,
+    { name: 'Ronaldo', goals: 28 },
+    { name: 'Neymar', goals: 22 },
+    { goals: 2 },
+    { name: 'Mbappé', goals: 25 },
+    { name: 'Pele', goals: null },
+];
+
+
+const checkPlayer = (player) => {
+    return player.filter(infor => infor && infor.name && infor.goals)
+}
+const result = checkPlayer(players);
+console.log(result);
+
+const checkMaxGoals = result.reduce((max, play) => max.goals > play.goals ? max : play, result[0]);
+console.log(checkMaxGoals);
