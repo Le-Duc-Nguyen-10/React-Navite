853f9fbc41072cf280c31939cbcf9498e135286f
Le-Duc-Nguyen-10 <nguyenldpd10357@gmail.com>
2025-01-08 10:38:13 +0700

    update project react

--- Lab1/app/(tabs)/index.tsx
+++ Lab1/app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome Le Duc Nguyen !</ThemedText>
+        <ThemedText type="title">Welcome Le Duc Nguyen to React Navite !</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
